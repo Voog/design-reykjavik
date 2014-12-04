147de81932aa9fca2bb8e535404d38a2a3d2699f
Kaspar Naaber <kaspar.naaber@outlook.com>
2014-12-04 14:49:44 +0200

    Update project structure
    Dont’t copy the third party javascripts to the javacsripts soruce
    folder and use the originals in bower_components/

--- javascripts/application.js
+++ javascripts/application.js
@@ -10307,7 +10307,212 @@ return jQuery;
 
 }));
 
-!function(a){a.fn.autogrow=function(){return this.filter("textarea").each(function(){var b=this,c=a(b),d=c.height(),e=c.hasClass("autogrow-short")?0:parseInt(c.css("lineHeight"))||0,f=a("<div></div>").css({position:"absolute",top:-1e4,left:-1e4,width:c.width(),fontSize:c.css("fontSize"),fontFamily:c.css("fontFamily"),fontWeight:c.css("fontWeight"),lineHeight:c.css("lineHeight"),resize:"none","word-wrap":"break-word"}).appendTo(document.body),g=function(a){var g=function(a,b){for(var c=0,d="";b>c;c++)d+=a;return d},h=b.value.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/&/g,"&amp;").replace(/\n$/,"<br/>&nbsp;").replace(/\n/g,"<br/>").replace(/ {2,}/g,function(a){return g("&nbsp;",a.length-1)+" "});a&&a.data&&"keydown"===a.data.event&&13===a.keyCode&&(h+="<br />"),f.css("width",c.width()),f.html(h+(0===e?"...":"")),c.height(Math.max(f.height()+e,d))};c.change(g).keyup(g).keydown({event:"keydown"},g),a(window).resize(g),g()})}}(jQuery);
+/*! Overthrow. An overflow:auto polyfill for responsive design. (c) 2012: Scott Jehl, Filament Group, Inc. http://filamentgroup.github.com/Overthrow/license.txt */
+(function( w, o, undefined ){
+
+	// o is overthrow reference from overthrow-polyfill.js
+	if( o === undefined ){
+		return;
+	}
+
+	o.scrollIndicatorClassName = "overthrow";
+	
+	var doc = w.document,
+		docElem = doc.documentElement,
+		// o api
+		nativeOverflow = o.support === "native",
+		canBeFilledWithPoly = o.canBeFilledWithPoly,
+		configure = o.configure,
+		set = o.set,
+		forget = o.forget,
+		scrollIndicatorClassName = o.scrollIndicatorClassName;
+
+	// find closest overthrow (elem or a parent)
+	o.closest = function( target, ascend ){
+		return !ascend && target.className && target.className.indexOf( scrollIndicatorClassName ) > -1 && target || o.closest( target.parentNode );
+	};
+		
+	// polyfill overflow
+	var enabled = false;
+	o.set = function(){
+			
+		set();
+
+		// If nativeOverflow or it doesn't look like the browser canBeFilledWithPoly, our job is done here. Exit viewport left.
+		if( enabled || nativeOverflow || !canBeFilledWithPoly ){
+			return;
+		}
+
+		w.overthrow.addClass();
+
+		enabled = true;
+
+		o.support = "polyfilled";
+
+		o.forget = function(){
+			forget();
+			enabled = false;
+			// Remove touch binding (check for method support since this part isn't qualified by touch support like the rest)
+			if( doc.removeEventListener ){
+				doc.removeEventListener( "touchstart", start, false );
+			}
+		};
+
+		// Fill 'er up!
+		// From here down, all logic is associated with touch scroll handling
+			// elem references the overthrow element in use
+		var elem,
+			
+			// The last several Y values are kept here
+			lastTops = [],
+	
+			// The last several X values are kept here
+			lastLefts = [],
+			
+			// lastDown will be true if the last scroll direction was down, false if it was up
+			lastDown,
+			
+			// lastRight will be true if the last scroll direction was right, false if it was left
+			lastRight,
+			
+			// For a new gesture, or change in direction, reset the values from last scroll
+			resetVertTracking = function(){
+				lastTops = [];
+				lastDown = null;
+			},
+			
+			resetHorTracking = function(){
+				lastLefts = [];
+				lastRight = null;
+			},
+		
+			// On webkit, touch events hardly trickle through textareas and inputs
+			// Disabling CSS pointer events makes sure they do, but it also makes the controls innaccessible
+			// Toggling pointer events at the right moments seems to do the trick
+			// Thanks Thomas Bachem http://stackoverflow.com/a/5798681 for the following
+			inputs,
+			setPointers = function( val ){
+				inputs = elem.querySelectorAll( "textarea, input" );
+				for( var i = 0, il = inputs.length; i < il; i++ ) {
+					inputs[ i ].style.pointerEvents = val;
+				}
+			},
+			
+			// For nested overthrows, changeScrollTarget restarts a touch event cycle on a parent or child overthrow
+			changeScrollTarget = function( startEvent, ascend ){
+				if( doc.createEvent ){
+					var newTarget = ( !ascend || ascend === undefined ) && elem.parentNode || elem.touchchild || elem,
+						tEnd;
+							
+					if( newTarget !== elem ){
+						tEnd = doc.createEvent( "HTMLEvents" );
+						tEnd.initEvent( "touchend", true, true );
+						elem.dispatchEvent( tEnd );
+						newTarget.touchchild = elem;
+						elem = newTarget;
+						newTarget.dispatchEvent( startEvent );
+					}
+				}
+			},
+			
+			// Touchstart handler
+			// On touchstart, touchmove and touchend are freshly bound, and all three share a bunch of vars set by touchstart
+			// Touchend unbinds them again, until next time
+			start = function( e ){
+
+				// Stop any throw in progress
+				if( o.intercept ){
+					o.intercept();
+				}
+				
+				// Reset the distance and direction tracking
+				resetVertTracking();
+				resetHorTracking();
+				
+				elem = o.closest( e.target );
+					
+				if( !elem || elem === docElem || e.touches.length > 1 ){
+					return;
+				}			
+
+				setPointers( "none" );
+				var touchStartE = e,
+					scrollT = elem.scrollTop,
+					scrollL = elem.scrollLeft,
+					height = elem.offsetHeight,
+					width = elem.offsetWidth,
+					startY = e.touches[ 0 ].pageY,
+					startX = e.touches[ 0 ].pageX,
+					scrollHeight = elem.scrollHeight,
+					scrollWidth = elem.scrollWidth,
+				
+					// Touchmove handler
+					move = function( e ){
+					
+						var ty = scrollT + startY - e.touches[ 0 ].pageY,
+							tx = scrollL + startX - e.touches[ 0 ].pageX,
+							down = ty >= ( lastTops.length ? lastTops[ 0 ] : 0 ),
+							right = tx >= ( lastLefts.length ? lastLefts[ 0 ] : 0 );
+							
+						// If there's room to scroll the current container, prevent the default window scroll
+						if( ( ty > 0 && ty < scrollHeight - height ) || ( tx > 0 && tx < scrollWidth - width ) ){
+							e.preventDefault();
+						}
+						// This bubbling is dumb. Needs a rethink.
+						else {
+							changeScrollTarget( touchStartE );
+						}
+						
+						// If down and lastDown are inequal, the y scroll has changed direction. Reset tracking.
+						if( lastDown && down !== lastDown ){
+							resetVertTracking();
+						}
+						
+						// If right and lastRight are inequal, the x scroll has changed direction. Reset tracking.
+						if( lastRight && right !== lastRight ){
+							resetHorTracking();
+						}
+						
+						// remember the last direction in which we were headed
+						lastDown = down;
+						lastRight = right;							
+						
+						// set the container's scroll
+						elem.scrollTop = ty;
+						elem.scrollLeft = tx;
+					
+						lastTops.unshift( ty );
+						lastLefts.unshift( tx );
+					
+						if( lastTops.length > 3 ){
+							lastTops.pop();
+						}
+						if( lastLefts.length > 3 ){
+							lastLefts.pop();
+						}
+					},
+				
+					// Touchend handler
+					end = function( e ){
+
+						// Bring the pointers back
+						setPointers( "auto" );
+						setTimeout( function(){
+							setPointers( "none" );
+						}, 450 );
+						elem.removeEventListener( "touchmove", move, false );
+						elem.removeEventListener( "touchend", end, false );
+					};
+				
+				elem.addEventListener( "touchmove", move, false );
+				elem.addEventListener( "touchend", end, false );
+			};
+			
+		// Bind to touch, handle move and end within
+		doc.addEventListener( "touchstart", start, false );
+	};
+		
+})( this, this.overthrow );
 
 // quantize.js, Copyright 2012 Shao-Chung Chen.
 // Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
@@ -11141,213 +11346,3 @@ MMCQ = (function() {
 
     init();
   })(jQuery);
-
-/*! Overthrow. An overflow:auto polyfill for responsive design. (c) 2012: Scott Jehl, Filament Group, Inc. http://filamentgroup.github.com/Overthrow/license.txt */
-(function( w, o, undefined ){
-
-	// o is overthrow reference from overthrow-polyfill.js
-	if( o === undefined ){
-		return;
-	}
-
-	o.scrollIndicatorClassName = "overthrow";
-	
-	var doc = w.document,
-		docElem = doc.documentElement,
-		// o api
-		nativeOverflow = o.support === "native",
-		canBeFilledWithPoly = o.canBeFilledWithPoly,
-		configure = o.configure,
-		set = o.set,
-		forget = o.forget,
-		scrollIndicatorClassName = o.scrollIndicatorClassName;
-
-	// find closest overthrow (elem or a parent)
-	o.closest = function( target, ascend ){
-		return !ascend && target.className && target.className.indexOf( scrollIndicatorClassName ) > -1 && target || o.closest( target.parentNode );
-	};
-		
-	// polyfill overflow
-	var enabled = false;
-	o.set = function(){
-			
-		set();
-
-		// If nativeOverflow or it doesn't look like the browser canBeFilledWithPoly, our job is done here. Exit viewport left.
-		if( enabled || nativeOverflow || !canBeFilledWithPoly ){
-			return;
-		}
-
-		w.overthrow.addClass();
-
-		enabled = true;
-
-		o.support = "polyfilled";
-
-		o.forget = function(){
-			forget();
-			enabled = false;
-			// Remove touch binding (check for method support since this part isn't qualified by touch support like the rest)
-			if( doc.removeEventListener ){
-				doc.removeEventListener( "touchstart", start, false );
-			}
-		};
-
-		// Fill 'er up!
-		// From here down, all logic is associated with touch scroll handling
-			// elem references the overthrow element in use
-		var elem,
-			
-			// The last several Y values are kept here
-			lastTops = [],
-	
-			// The last several X values are kept here
-			lastLefts = [],
-			
-			// lastDown will be true if the last scroll direction was down, false if it was up
-			lastDown,
-			
-			// lastRight will be true if the last scroll direction was right, false if it was left
-			lastRight,
-			
-			// For a new gesture, or change in direction, reset the values from last scroll
-			resetVertTracking = function(){
-				lastTops = [];
-				lastDown = null;
-			},
-			
-			resetHorTracking = function(){
-				lastLefts = [];
-				lastRight = null;
-			},
-		
-			// On webkit, touch events hardly trickle through textareas and inputs
-			// Disabling CSS pointer events makes sure they do, but it also makes the controls innaccessible
-			// Toggling pointer events at the right moments seems to do the trick
-			// Thanks Thomas Bachem http://stackoverflow.com/a/5798681 for the following
-			inputs,
-			setPointers = function( val ){
-				inputs = elem.querySelectorAll( "textarea, input" );
-				for( var i = 0, il = inputs.length; i < il; i++ ) {
-					inputs[ i ].style.pointerEvents = val;
-				}
-			},
-			
-			// For nested overthrows, changeScrollTarget restarts a touch event cycle on a parent or child overthrow
-			changeScrollTarget = function( startEvent, ascend ){
-				if( doc.createEvent ){
-					var newTarget = ( !ascend || ascend === undefined ) && elem.parentNode || elem.touchchild || elem,
-						tEnd;
-							
-					if( newTarget !== elem ){
-						tEnd = doc.createEvent( "HTMLEvents" );
-						tEnd.initEvent( "touchend", true, true );
-						elem.dispatchEvent( tEnd );
-						newTarget.touchchild = elem;
-						elem = newTarget;
-						newTarget.dispatchEvent( startEvent );
-					}
-				}
-			},
-			
-			// Touchstart handler
-			// On touchstart, touchmove and touchend are freshly bound, and all three share a bunch of vars set by touchstart
-			// Touchend unbinds them again, until next time
-			start = function( e ){
-
-				// Stop any throw in progress
-				if( o.intercept ){
-					o.intercept();
-				}
-				
-				// Reset the distance and direction tracking
-				resetVertTracking();
-				resetHorTracking();
-				
-				elem = o.closest( e.target );
-					
-				if( !elem || elem === docElem || e.touches.length > 1 ){
-					return;
-				}			
-
-				setPointers( "none" );
-				var touchStartE = e,
-					scrollT = elem.scrollTop,
-					scrollL = elem.scrollLeft,
-					height = elem.offsetHeight,
-					width = elem.offsetWidth,
-					startY = e.touches[ 0 ].pageY,
-					startX = e.touches[ 0 ].pageX,
-					scrollHeight = elem.scrollHeight,
-					scrollWidth = elem.scrollWidth,
-				
-					// Touchmove handler
-					move = function( e ){
-					
-						var ty = scrollT + startY - e.touches[ 0 ].pageY,
-							tx = scrollL + startX - e.touches[ 0 ].pageX,
-							down = ty >= ( lastTops.length ? lastTops[ 0 ] : 0 ),
-							right = tx >= ( lastLefts.length ? lastLefts[ 0 ] : 0 );
-							
-						// If there's room to scroll the current container, prevent the default window scroll
-						if( ( ty > 0 && ty < scrollHeight - height ) || ( tx > 0 && tx < scrollWidth - width ) ){
-							e.preventDefault();
-						}
-						// This bubbling is dumb. Needs a rethink.
-						else {
-							changeScrollTarget( touchStartE );
-						}
-						
-						// If down and lastDown are inequal, the y scroll has changed direction. Reset tracking.
-						if( lastDown && down !== lastDown ){
-							resetVertTracking();
-						}
-						
-						// If right and lastRight are inequal, the x scroll has changed direction. Reset tracking.
-						if( lastRight && right !== lastRight ){
-							resetHorTracking();
-						}
-						
-						// remember the last direction in which we were headed
-						lastDown = down;
-						lastRight = right;							
-						
-						// set the container's scroll
-						elem.scrollTop = ty;
-						elem.scrollLeft = tx;
-					
-						lastTops.unshift( ty );
-						lastLefts.unshift( tx );
-					
-						if( lastTops.length > 3 ){
-							lastTops.pop();
-						}
-						if( lastLefts.length > 3 ){
-							lastLefts.pop();
-						}
-					},
-				
-					// Touchend handler
-					end = function( e ){
-
-						// Bring the pointers back
-						setPointers( "auto" );
-						setTimeout( function(){
-							setPointers( "none" );
-						}, 450 );
-						elem.removeEventListener( "touchmove", move, false );
-						elem.removeEventListener( "touchend", end, false );
-					};
-				
-				elem.addEventListener( "touchmove", move, false );
-				elem.addEventListener( "touchend", end, false );
-			};
-			
-		// Bind to touch, handle move and end within
-		doc.addEventListener( "touchstart", start, false );
-	};
-		
-})( this, this.overthrow );
-
-/* Placeholders.js v3.0.2 */
-(function(t){"use strict";function e(t,e,r){return t.addEventListener?t.addEventListener(e,r,!1):t.attachEvent?t.attachEvent("on"+e,r):void 0}function r(t,e){var r,n;for(r=0,n=t.length;n>r;r++)if(t[r]===e)return!0;return!1}function n(t,e){var r;t.createTextRange?(r=t.createTextRange(),r.move("character",e),r.select()):t.selectionStart&&(t.focus(),t.setSelectionRange(e,e))}function a(t,e){try{return t.type=e,!0}catch(r){return!1}}t.Placeholders={Utils:{addEventListener:e,inArray:r,moveCaret:n,changeType:a}}})(this),function(t){"use strict";function e(){}function r(){try{return document.activeElement}catch(t){}}function n(t,e){var r,n,a=!!e&&t.value!==e,u=t.value===t.getAttribute(V);return(a||u)&&"true"===t.getAttribute(D)?(t.removeAttribute(D),t.value=t.value.replace(t.getAttribute(V),""),t.className=t.className.replace(R,""),n=t.getAttribute(F),parseInt(n,10)>=0&&(t.setAttribute("maxLength",n),t.removeAttribute(F)),r=t.getAttribute(P),r&&(t.type=r),!0):!1}function a(t){var e,r,n=t.getAttribute(V);return""===t.value&&n?(t.setAttribute(D,"true"),t.value=n,t.className+=" "+I,r=t.getAttribute(F),r||(t.setAttribute(F,t.maxLength),t.removeAttribute("maxLength")),e=t.getAttribute(P),e?t.type="text":"password"===t.type&&M.changeType(t,"text")&&t.setAttribute(P,"password"),!0):!1}function u(t,e){var r,n,a,u,i,l,o;if(t&&t.getAttribute(V))e(t);else for(a=t?t.getElementsByTagName("input"):b,u=t?t.getElementsByTagName("textarea"):f,r=a?a.length:0,n=u?u.length:0,o=0,l=r+n;l>o;o++)i=r>o?a[o]:u[o-r],e(i)}function i(t){u(t,n)}function l(t){u(t,a)}function o(t){return function(){m&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)?M.moveCaret(t,0):n(t)}}function c(t){return function(){a(t)}}function s(t){return function(e){return A=t.value,"true"===t.getAttribute(D)&&A===t.getAttribute(V)&&M.inArray(C,e.keyCode)?(e.preventDefault&&e.preventDefault(),!1):void 0}}function d(t){return function(){n(t,A),""===t.value&&(t.blur(),M.moveCaret(t,0))}}function g(t){return function(){t===r()&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)&&M.moveCaret(t,0)}}function v(t){return function(){i(t)}}function p(t){t.form&&(T=t.form,"string"==typeof T&&(T=document.getElementById(T)),T.getAttribute(U)||(M.addEventListener(T,"submit",v(T)),T.setAttribute(U,"true"))),M.addEventListener(t,"focus",o(t)),M.addEventListener(t,"blur",c(t)),m&&(M.addEventListener(t,"keydown",s(t)),M.addEventListener(t,"keyup",d(t)),M.addEventListener(t,"click",g(t))),t.setAttribute(j,"true"),t.setAttribute(V,x),(m||t!==r())&&a(t)}var b,f,m,h,A,y,E,x,L,T,N,S,w,B=["text","search","url","tel","email","password","number","textarea"],C=[27,33,34,35,36,37,38,39,40,8,46],k="#ccc",I="placeholdersjs",R=RegExp("(?:^|\\s)"+I+"(?!\\S)"),V="data-placeholder-value",D="data-placeholder-active",P="data-placeholder-type",U="data-placeholder-submit",j="data-placeholder-bound",q="data-placeholder-focus",z="data-placeholder-live",F="data-placeholder-maxlength",G=document.createElement("input"),H=document.getElementsByTagName("head")[0],J=document.documentElement,K=t.Placeholders,M=K.Utils;if(K.nativeSupport=void 0!==G.placeholder,!K.nativeSupport){for(b=document.getElementsByTagName("input"),f=document.getElementsByTagName("textarea"),m="false"===J.getAttribute(q),h="false"!==J.getAttribute(z),y=document.createElement("style"),y.type="text/css",E=document.createTextNode("."+I+" { color:"+k+"; }"),y.styleSheet?y.styleSheet.cssText=E.nodeValue:y.appendChild(E),H.insertBefore(y,H.firstChild),w=0,S=b.length+f.length;S>w;w++)N=b.length>w?b[w]:f[w-b.length],x=N.attributes.placeholder,x&&(x=x.nodeValue,x&&M.inArray(B,N.type)&&p(N));L=setInterval(function(){for(w=0,S=b.length+f.length;S>w;w++)N=b.length>w?b[w]:f[w-b.length],x=N.attributes.placeholder,x?(x=x.nodeValue,x&&M.inArray(B,N.type)&&(N.getAttribute(j)||p(N),(x!==N.getAttribute(V)||"password"===N.type&&!N.getAttribute(P))&&("password"===N.type&&!N.getAttribute(P)&&M.changeType(N,"text")&&N.setAttribute(P,"password"),N.value===N.getAttribute(V)&&(N.value=x),N.setAttribute(V,x)))):N.getAttribute(D)&&(n(N),N.removeAttribute(V));h||clearInterval(L)},100)}M.addEventListener(t,"beforeunload",function(){K.disable()}),K.disable=K.nativeSupport?e:i,K.enable=K.nativeSupport?e:l}(this);
